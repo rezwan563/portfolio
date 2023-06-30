97cb6bec9fa37044b11a85b7bdeb33708a5aea56
Rezwan <t.rewan17@gmail.com>
2023-06-30 15:35:28 +0600

    Created purple dot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import {motion} from 'framer-motion'
 
+import {styles} from '../style'
+import { ComputersCanvas } from './canvas'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className='flexx flex-col justify-center items-center mt-5'>
+        <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+      </div>
+    </div>
+    </section>
   )
 }
 
